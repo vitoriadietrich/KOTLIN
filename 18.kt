db932ea74a7b1dd0c66ed7b475f29d8e0ae17993
Vitoria de Jesus Dietrich <167101256+vitoriadietrich@users.noreply.github.com>
2025-03-03 17:11:14 -0300

    Add files via upload

---
+++ 18.kt
@@ -0,0 +1,15 @@
+fun main(){
+   // Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em
+  //ordem decrescente.
+ //Exemplo: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+    var decrescente = 0
+            decrescente = 10
+
+    // Laço enquanto para imprimir os números de 10 a 1
+    while(decrescente >= 1)
+    {
+        println(decrescente)  // Imprime o valor de decrescente
+        decrescente--  // Decrementa 1 em decrescente
+    }
+}
