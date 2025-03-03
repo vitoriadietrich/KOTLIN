da0b658413739e17a138ebefa247a3f3dc93db26
Vitoria de Jesus Dietrich <167101256+vitoriadietrich@users.noreply.github.com>
2025-03-03 13:21:10 -0300

    Add files via upload

---
+++ 2.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+
+//Escreva um programa em que o usuário informe o seu nome e exiba a mensagem
+// "Olá, [NomeDoUsuario]".
+
+    val nomeDoUsuario = "Vitória de Jesus Dietrich"
+    println("Olá, $nomeDoUsuario")
+
+}
