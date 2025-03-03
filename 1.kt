02dfa42950a1a14ba1880514489cb600dcaabc12
Vitoria de Jesus Dietrich <167101256+vitoriadietrich@users.noreply.github.com>
2025-03-03 13:05:20 -0300

    Add files via upload

---
+++ 1.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+ // Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a
+// um valor "Fusca". Exiba-a ao usuário.
+
+    val nomeDoCarro = "Fusca"
+    println(nomeDoCarro)
+
+}
